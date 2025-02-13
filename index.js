b8e8d0da48633eaea162c86c402389401ed0a96b
Marlon Silva <marlonsilvavvv@gmail.com>
2025-02-13 11:52:24 -0300

    testcexpress

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const venom = require('venom-bot');
 
-venom
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+  venom
   .create({
     session: 'meu-bot', // Nome da sessão
     headless: true, // Impede a abertura do navegador
@@ -16,3 +21,24 @@ function start(client) {
     }
   });
 }
+
+
+app.get('/', (req, res) => {
+  res.send('Venom-Bot está rodando!');
+});
+
+app.get('/send/:number/:message', async (req, res) => {
+  const { number, message } = req.params;
+  try {
+    await client.sendText(`${number}@c.us`, message);
+    res.send('Mensagem enviada!');
+  } catch (error) {
+    res.status(500).send('Erro ao enviar mensagem!');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+
